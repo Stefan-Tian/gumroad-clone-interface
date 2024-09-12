a434fc4875dc3e65b7da4640fede61c9edfe3419
Sheng Hong Tian <stefan55831@gmail.com>
2024-09-12 18:29:44 +0800

    Remove unused darkmode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class', // or 'media' if you prefer to use the user's system preference
   content: [
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
